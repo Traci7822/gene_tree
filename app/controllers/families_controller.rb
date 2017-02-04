401e2d679bf45520b0129191d969259e35580ed0
Traci7822 <Tracir@gmail.com>
2017-02-04 15:49:54 -0700

    #create functioning and routing

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -6,11 +6,18 @@ class FamiliesController < ApplicationController
   end
 
   def create
-    @family = Family.new(params[:family])
+    @family = Family.create(family_params)
     @family.users << current_user
-
+    
     if @family.save
+      redirect_to root_path
     end
+  end
+
+  private
+
+  def family_params
+    params.require(:family).permit(:name, :motto, :location, :origin_location, :start_year)
   end
 
 end
