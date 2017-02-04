7f2ee00fbf35789bf48f68e8f2718416b55f28f8
Traci7822 <Tracir@gmail.com>
2017-02-04 14:45:34 -0700

    families#create with authentication

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,2 +1,12 @@
 class FamiliesController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+  
+  def create
+    @family = Family.new(params[:family])
+    @family.users << current_user
+
+    if @family.save
+    end
+  end
+
 end
